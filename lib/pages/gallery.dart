f64ace38b5524a858b1c22eacfd749dcc270626a
江旭晨 <jiangxuchen@kouakiraashitas-MacBook-puro.local>
2020-05-02 18:39:04 -0700

    change gallery

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -10,42 +10,86 @@ class _GalleryState extends State<Gallery> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: new AppBar(
-          backgroundColor: Colors.black,
-          title: new Text('Gallery'),
-          centerTitle: true,
-          elevation: 0,
+        appBar: PreferredSize(
+          child: AppBar(
+            leading: new Container(),
+            backgroundColor: Colors.white,
+            // status bar color
+            brightness: Brightness.light,
+            elevation: 0.0,
+          ),
+          preferredSize: Size.fromHeight(10.0),
         ),
         body: Container(
-          padding: EdgeInsets.fromLTRB(24.0, 30.0, 24.0, 30.0),
-          child: ListView.builder(
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Time',
+          padding: EdgeInsets.fromLTRB(24.0, 0, 24.0, 30.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Gallery",
                     style: TextStyle(
                         fontFamily: 'CantataOne',
                         fontSize: 30,
                         fontWeight: FontWeight.w400,
                         color: Colors.black),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 10),
-                    height: 200,
-                    decoration: BoxDecoration(
-                        border: Border(
-                            left: BorderSide(
-                      width: 3,
-                      color: Colors.black,
-                    ))),
-                    child: Text("Content"),
-                  )
-                ],
-              );
-            },
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                  height: 670.0,
+                  child: new ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return new Stack(
+                        children: <Widget>[
+                          new Padding(
+                            padding: const EdgeInsets.only(left: 50.0),
+                            child: new Card(
+                              margin: new EdgeInsets.all(20.0),
+                              child: new Container(
+                                width: double.infinity,
+                                height: 200.0,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                          new Positioned(
+                            top: 0.0,
+                            bottom: 0.0,
+                            left: 35.0,
+                            child: new Container(
+                              height: double.infinity,
+                              width: 1.0,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          new Positioned(
+                            top: 100.0,
+                            left: 15.0,
+                            child: new Container(
+                              height: 40.0,
+                              width: 40.0,
+                              decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: new Container(
+                                margin: new EdgeInsets.all(5.0),
+                                height: 30.0,
+                                width: 30.0,
+                                decoration: new BoxDecoration(
+                                    shape: BoxShape.circle, color: Colors.red),
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                    itemCount: 5,
+                  )),
+            ],
           ),
         ));
   }
