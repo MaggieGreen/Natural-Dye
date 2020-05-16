e72f421b9afabe30f08b63602ef2bb09b0ddfa99
江旭晨 <jiangxuchen@kouakiraashitas-MacBook-puro.local>
2020-05-15 23:18:14 -0700

    plant detail page

--- lib/pages/plant_detail.dart
+++ lib/pages/plant_detail.dart
@@ -6,6 +6,30 @@ class MainCollapsingToolbar extends StatefulWidget {
 }
 
 class _MainCollapsingToolbarState extends State<MainCollapsingToolbar> {
+  void _showModalSheet() {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        builder: (builder) {
+          return new Container(
+              height: MediaQuery.of(context).size.height * 0.75,
+              // padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+              color: Colors.transparent,
+              child: new Container(
+                decoration: new BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(15.0),
+                    topRight: const Radius.circular(50.0),
+                  ),
+                ),
+                child: Container(
+                  child: Column(),
+                ),
+              ));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +49,14 @@ class _MainCollapsingToolbarState extends State<MainCollapsingToolbar> {
                       color: Colors.white,
                       fontSize: 24.0,
                     )),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.favorite_border),
+                    onPressed: () {
+                      print('saved');
+                    },
+                  )
+                ],
                 flexibleSpace: FlexibleSpaceBar(
                     background: Image.asset(
                   'assets/images/testplant.png',
@@ -54,7 +86,7 @@ class _MainCollapsingToolbarState extends State<MainCollapsingToolbar> {
               children: <Widget>[
                 new Container(
                   height: 20.0,
-                  color: Colors.amber,
+                  color: Colors.white,
                 ),
                 new Container(
                   height: 20.0,
@@ -63,10 +95,57 @@ class _MainCollapsingToolbarState extends State<MainCollapsingToolbar> {
                 new Container(
                   height: 20.0,
                   color: Colors.white,
-                )
+                ),
               ],
             ),
           ),
+        ),
+      ),
+      //Add to material list
+      bottomNavigationBar: Container(
+        height: 56,
+        padding: EdgeInsets.only(left: 20.0),
+        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 50,
+                child: FlatButton(
+                    onPressed: () =>
+                        {Navigator.pushNamed(context, '/colorpicker')},
+                    child: Text(
+                      "Add to material list",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.pink,
+                      Colors.pinkAccent,
+                      Colors.purpleAccent
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+              ),
+              flex: 3,
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: _showModalSheet,
+                color: Color(0xFF323232),
+                child: Icon(
+                  Icons.shopping_basket,
+                  color: Colors.white,
+                ),
+                padding: EdgeInsets.all(14),
+                shape: CircleBorder(),
+              ),
+            )
+          ],
         ),
       ),
     );
