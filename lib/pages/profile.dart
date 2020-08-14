490ede4263ff480f7073b7dc249cbfaa9d45d794
江旭晨 <jiangxuchen@kouakiraashitas-MacBook-puro.local>
2020-08-13 21:19:00 -0700

    homepage done(in profile page)

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'CustomShapeClipper.dart';
 
-Color firstColor = Color(0xFFF47D15);
-Color secondColor = Color(0xFFEF772C);
+Color firstColor = Color.fromRGBO(255, 255, 255, 0.5);
+Color secondColor = Color.fromRGBO(255, 255, 255, 0.2);
 
-List<String> location = ['Boston(Bos)', 'New York City(JFK)'];
+List<String> location = [
+  'San Francisco(SF)',
+  'Texas(TX)',
+  'New York City(JFK)'
+];
 
 const TextStyle dropDownLabeStyle =
-    TextStyle(color: Colors.white, fontSize: 16.0);
+    TextStyle(color: Colors.black, fontSize: 16.0);
 
 const TextStyle dropDownMeunItemStyle =
     TextStyle(color: Colors.black, fontSize: 16.0);
@@ -27,19 +31,22 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        child: AppBar(
-          leading: new Container(),
-          backgroundColor: Colors.white,
-          // status bar color
-          brightness: Brightness.light,
-          elevation: 0.0,
-        ),
-        preferredSize: Size.fromHeight(10.0),
-      ),
+      // appBar: PreferredSize(
+      //   child: AppBar(
+      //     leading: new Container(),
+      //     backgroundColor: Colors.white,
+      //     // status bar color
+      //     brightness: Brightness.light,
+      //     elevation: 0.0,
+      //   ),
+      //   preferredSize: Size.fromHeight(10.0),
+      // ),
       body: Column(
         children: [
           HomeScreenTopPart(),
+          SizedBox(
+            height: 40.0,
+          ),
           homeScreenBottomPart,
         ],
       ),
@@ -57,9 +64,17 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        ClipPath(
-          //cruved background
-          clipper: CustomShapeClipper(),
+        Container(
+          //Shape background need change Container to clip path
+          // clipper: CustomShapeClipper(),
+          //Image background
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: const Radius.circular(25.0),
+                  bottomRight: const Radius.circular(25.0)),
+              image: DecorationImage(
+                  image: AssetImage('assets/images/testcity.png'),
+                  fit: BoxFit.cover)),
           //location and search bar
           child: Container(
             height: 292.0,
@@ -76,7 +91,7 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                     children: [
                       Icon(
                         Icons.location_on,
-                        color: Colors.white,
+                        color: Colors.black,
                       ),
                       SizedBox(
                         width: 16.0,
@@ -95,7 +110,7 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                             ),
                             Icon(
                               Icons.keyboard_arrow_down,
-                              color: Colors.white,
+                              color: Colors.black,
                             )
                           ],
                         ),
@@ -111,6 +126,13 @@ class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
                           PopupMenuItem(
                             child: Text(
                               location[1],
+                              style: dropDownMeunItemStyle,
+                            ),
+                            value: 1,
+                          ),
+                          PopupMenuItem(
+                            child: Text(
+                              location[2],
                               style: dropDownMeunItemStyle,
                             ),
                             value: 1,
@@ -206,16 +228,18 @@ var homeScreenBottomPart = Column(
 );
 
 List<PlantCard> plantCards = [
-  PlantCard("assets/images/Inspiration8.jpg", "Marigold", "Easy", "40min"),
+  PlantCard("assets/images/Inspiration8.jpg", "Marigold", "Easy", "40min",
+      "onTap: () => {Navigator.pushNamed(context, '/plantdetail')}"),
   PlantCard(
-      "assets/images/Inspiration1.jpg", "Elderberry", "Middle", "1h40min"),
-  PlantCard("assets/images/Inspiration4.jpg", "Avocado", "Middle", "1week"),
+      "assets/images/Inspiration1.jpg", "Elderberry", "Middle", "1h40min", ""),
+  PlantCard("assets/images/Inspiration4.jpg", "Avocado", "Middle", "1week", ""),
 ];
 
 class PlantCard extends StatelessWidget {
-  final String imagePath, plantName, level, time;
+  final String imagePath, plantName, level, time, dynamic;
 
-  PlantCard(this.imagePath, this.plantName, this.level, this.time);
+  PlantCard(
+      this.imagePath, this.plantName, this.level, this.time, this.dynamic);
 
   @override
   Widget build(BuildContext context) {
@@ -225,12 +249,15 @@ class PlantCard extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(20.0)),
         child: Stack(
           children: [
-            Container(
-              height: 380.0,
-              width: 210.0,
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.cover,
+            InkWell(
+              onTap: () => {Navigator.pushNamed(context, '/plantdetail')},
+              child: Container(
+                height: 380.0,
+                width: 210.0,
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             Positioned.fill(
